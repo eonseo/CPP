78eca3c27872f352a66a9b1c0d78355599824d25
Eonseo <139443553+eonseo@users.noreply.github.com>
2025-01-02 22:48:28 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.cc
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.cc
@@ -1,19 +1,6 @@
 #include <iostream>
 using namespace std;
 
-class Swap
-{
-	int tmp;
-
-public:
-	void swap(int *a, int *b)
-	{
-		tmp = *a;
-		*a = *b;
-		*b = tmp;
-	}
-};
-
 int main()
 {
 	ios::sync_with_stdio(false);
@@ -31,21 +18,17 @@ int main()
 	for (int i = 0; i < n; i++)
 		arr[i] = i + 1;
 	
-	for (int i = 0; i < m; i++)
+	while(m--)
 	{
 		int a, b;
-		Swap swap;
 
 		cin >> a >> b;
-		for (int j = 0; j <= ((b - a - 1) / 2); j++)
-		{
-			if (a != b)
-				swap.swap(&arr[a - 1 + j], &arr[(b - 1 - j)]);
-		}
+		for (int i = 0; i <= ((b - a - 1) / 2); i++)
+			swap(arr[a - 1 + i], arr[(b - 1 - i)]);
 	}
 
 	for (int i = 0; i < n; i++)
-		cout << arr[i] << " ";
+		cout << arr[i] << ' ';
 	
 	return 0;
 }
