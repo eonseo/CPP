1f27795e5508c86bfb07e732f21d64baa59dcf22
Eonseo <139443553+eonseo@users.noreply.github.com>
2025-05-12 21:22:39 +0900

    [Silver I] Title: 정수 삼각형, Time: 8 ms, Memory: 2548 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
@@ -11,30 +11,24 @@ void	free2dimensionalArr(int **arr) {
 }
 
 int Solution(int **arr) {
-	int **dp = new int*[n];
 	int sumMax = 0;
-	for (int i = 0; i < n; i++) {
-		dp[i] = new int[i + 1];
-	}
-	dp[0][0] = arr[0][0];
 	if (n >= 2) {
-		dp[1][0] = dp[0][0] + arr[1][0];
-		dp[1][1] = dp[0][0] + arr[1][1];
+		arr[1][0] = arr[0][0] + arr[1][0];
+		arr[1][1] = arr[0][0] + arr[1][1];
 	}
 	for (int i = 2; i < n; i++) {
 		for (int j = 0; j <= i; j++) {
 			if (j == 0)
-				dp[i][j] = dp[i - 1][j] + arr[i][j];
+				arr[i][j] = arr[i - 1][j] + arr[i][j];
 			else if (j == i)
-				dp[i][j] = dp[i - 1][j - 1] + arr[i][j];
+				arr[i][j] = arr[i - 1][j - 1] + arr[i][j];
 			else
-				dp[i][j] = max(dp[i - 1][j - 1], dp[i - 1][j]) + arr[i][j];
+				arr[i][j] = max(arr[i - 1][j - 1], arr[i - 1][j]) + arr[i][j];
 		}
 	}
 	for (int i = 0; i < n; i++) {
-		sumMax = max(sumMax, dp[n - 1][i]);
+		sumMax = max(sumMax, arr[n - 1][i]);
 	}
-	free2dimensionalArr(dp);
 	return sumMax;
 }
 
